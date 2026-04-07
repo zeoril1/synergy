f8235130fa0e90d6e82e9ef11d16bb4291fa0bbf
zeoril <neprim1@yandex.ru>
2026-04-07 14:03:09 +0300

    3 lesson

---
+++ 3_lesson/1.py
@@ -0,0 +1,8 @@
+print("Введите вид питомца: ")
+typeAnimal = input()
+print("Введите кличку питомца: ")
+nameAnimal = input()
+print("Введите возвраст питомца: ")
+ageAnimal = input()
+
+print('Это %s по кличке "%s". Возраст: %s года.' % (typeAnimal, ageAnimal, nameAnimal))
